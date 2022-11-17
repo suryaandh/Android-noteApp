277b07d138616d401146c5c4476a0bbc21e6e225
suryaant <gdswiyasa@gmail.com>
2022-11-17 22:40:08 +0800

    main activity on progress

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -2,8 +2,19 @@ package com.example.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notesapp.Adapter.NoteAdapter
+import com.example.notesapp.Database.NoteDatabase
+import com.example.notesapp.Models.Note
+import com.example.notesapp.Models.NoteViewModel
+import com.example.notesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var database: NoteDatabase
+    lateinit var viewModel: NoteViewModel
+    lateinit var adapter: NoteAdapter
+    lateinit var selectedNote: Note
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
