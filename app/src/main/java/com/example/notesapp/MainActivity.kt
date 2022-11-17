c12b8fb425a1ed946c66db693c1ded529dce1175
suryaant <gdswiyasa@gmail.com>
2022-11-18 00:52:08 +0800

    Activity main update view model

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.notesapp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.notesapp.Adapter.NoteAdapter
 import com.example.notesapp.Database.NoteDatabase
 import com.example.notesapp.Models.Note
@@ -15,8 +20,41 @@ class MainActivity : AppCompatActivity() {
     lateinit var viewModel: NoteViewModel
     lateinit var adapter: NoteAdapter
     lateinit var selectedNote: Note
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Initialize UI
+        initUI()
+
+        viewModel =ViewModelProvider(this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel::class.java)
+
+        viewModel.allNotes.observe(this){list->
+            list?.let {
+                adapter.updateList(list)
+            }
+        }
+        database= NoteDatabase.getDatabase(this)
+    }
+
+    private fun initUI(){
+        binding.recycleView.setHasFixedSize(true)
+        binding.recycleView.layoutManager = StaggeredGridLayoutManager(2, LinearLayout.VERTICAL)
+        adapter=NoteAdapter(this,this)
+        binding.recycleView.adapter=adapter
+
+        val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+            if( result.resultCode==Activity.RESULT_OK){
+                val note = result.data?.getSerializableExtra('note') as? Note
+                if (note!=null){
+
+                    viewModel.insertNote(note)
+                }
+            }
+
+        }
     }
 }
