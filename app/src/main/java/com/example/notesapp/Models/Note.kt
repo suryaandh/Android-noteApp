cb8b5f03e66d7a7cf07b56424a85ce2980cc940c
suryaant <gdswiyasa@gmail.com>
2022-11-18 11:25:01 +0800

    import java io

--- app/src/main/java/com/example/notesapp/Models/Note.kt
+++ app/src/main/java/com/example/notesapp/Models/Note.kt
@@ -6,8 +6,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName="notes_table")
 data class Note(
-@PrimaryKey(autoGenerate = true) val id: Int?,
+    @PrimaryKey(autoGenerate = true) val id: Int?,
     @ColumnInfo(name="title")val title:String?,
-            @ColumnInfo(name = "note")val note:String?,
-                    @ColumnInfo(name = "date") val date:String?
-)
+    @ColumnInfo(name = "note")val note:String?,
+    @ColumnInfo(name = "date") val date:String?
+):java.io.Serializable
